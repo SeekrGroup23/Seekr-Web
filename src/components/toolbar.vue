6a70e481a0f17bd1efbb0080ac95cee3af028bbf
Pasindu Dewapriya <lapjanith@gmail.com>
2019-09-06 10:25:52 +0530

    Added User Login - UI

---
+++ src/components/toolbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-toolbar app>
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>
+      <!-- <v-img src="../assets/logo.png" alt="logo" aspect-ratio="1"></v-img> -->
+      <img class="logo" src="../assets/logo.png" alt="" />
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat>Link One</v-btn>
+      <v-btn flat>Link Two</v-btn>
+      <v-btn flat>Link Three</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.logo {
+  width: 15%;
+  height: 8%;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,130 @@
+<template>
+  <v-container>
+    <v-layout justify-center>
+      <v-flex xs12 sm6 md4 row>
+        <v-card height="500" class="mt-4">
+          <v-card-title primary-title>
+            <h2>Login</h2>
+          </v-card-title>
+          <v-card-text>
+            <v-text-field
+              v-model="email"
+              :error-messages="emailErrors"
+              outline
+              label="Email"
+              append-icon="email"
+              required
+              @input="$v.email.$touch()"
+              @blur="$v.email.$touch()"
+            ></v-text-field>
+
+            <v-text-field
+              v-model="password"
+              :error-messages="passwordErrors"
+              outline
+              label="Password"
+              append-icon="lock"
+              required
+              @input="$v.password.$touch()"
+              @blur="$v.password.$touch()"
+            ></v-text-field>
+
+            <span class="forg-pass mt-0"><a href="#">Forgot Password</a></span>
+          </v-card-text>
+          <v-card-actions>
+            <v-flex class="mx-1 px-1">
+              <v-btn
+                v-on:click="login"
+                class="btn-login mb-2"
+                Large
+                color="primary"
+                >Login</v-btn
+              >
+              <p class="mx-1 mt-3">Don't Have an Account?</p>
+
+              <v-btn class="btn-login" Large>Register</v-btn>
+            </v-flex>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import { validationMixin } from "vuelidate";
+import { required, email } from "vuelidate/lib/validators";
+
+const axios = require("axios");
+
+export default {
+  mixins: [validationMixin],
+  validations: {
+    email: { required, email },
+    password: { required }
+  },
+
+  data: () => ({
+    email: "",
+    password: ""
+  }),
+
+  computed: {
+    emailErrors() {
+      const errors = [];
+      if (!this.$v.email.$dirty) return errors;
+      !this.$v.email.email && errors.push("Must be valid e-mail");
+      !this.$v.email.required && errors.push("E-mail is required");
+      return errors;
+    },
+    passwordErrors() {
+      const errors = [];
+      if (!this.$v.password.$dirty) return errors;
+      !this.$v.password.required && errors.push("Password is required");
+      return errors;
+    }
+  },
+
+  methods: {
+    //Form Validation
+    submit() {
+      this.$v.$touch();
+    },
+    //Login Method -> Sends HTTP POST request to the server to verify the user
+    login() {
+      axios
+        .post("http://localhost:5000/api/login", {
+          firstName: "Fred",
+          lastName: "Flintstone"
+        })
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.btn-login {
+  width: 100%;
+}
+
+.btn-login-cover {
+  padding-left: 8px;
+  padding-right: 8px;
+  width: 100%;
+}
+
+.forg-pass {
+  padding-left: 5px;
+}
+
+.forg-pass a {
+  text-decoration: none;
+  color: #455a64;
+}
+</style>
