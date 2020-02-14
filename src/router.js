dc3c3fbf92e4111cd4b25e5ae9c9cd2c0aaf918d
nirajGunawardana <47717506+nirajGunawardana@users.noreply.github.com>
2020-02-14 21:59:14 +0530

    CREATE ROUTING FOR SHOW_POST,NEWS,CREATEPOST

--- src/router.js
+++ src/router.js
@@ -1,38 +1,42 @@
 import Vue from "vue";
+
 import Router from "vue-router";
+
 import store from "./store/store";
-import Home from "./views/Home/Home.vue";
 import Login from "./views/Login.vue";
 import Register from "./views/Register.vue";
-import GramaNiladhari from "./views/GramaNiladhari/GramaNiladhari.vue";
-import GN_Dashboard from "./views/GramaNiladhari/Dashboard.vue";
-import GN_Profile from "./views/GramaNiladhari/Profile.vue";
-import GN_Patients from "./views/GramaNiladhari/Patients.vue";
-import User_Profile from "./views/User/Profile.vue";
-import Questionnaire from "./views/Questionnaire/Questionnaire_A.vue";
-import MedicalOfficer from "./views/MedicalOfficer/Doctor.vue";
-import MO_Dashboard from "./views/MedicalOfficer/Dashboard.vue";
-import MO_AddPatient from "./views/MedicalOfficer/AddPatient.vue";
-import MO_ViewAllPatients from "./views/MedicalOfficer/ViewAllPatients.vue";
-import MO_Profile from "./views/MedicalOfficer/Profile.vue";
 import Admin from "./views/Admin/Admin.vue";
 import Admin_Dashboard from "./views/Admin/Dashboard.vue";
-import Admin_Patients_ViewAll from "./views/Admin/Patients/ViewAll.vue";
-import Admin_Patients_AddNew from "./views/Admin/Patients/AddNew.vue";
-import Admin_MO_AddNew from "./views/Admin/MedicalOfficers/AddNew.vue";
-import Admin_MO_ViewAll from "./views/Admin/MedicalOfficers/ViewAll.vue";
-import Admin_Hospital_ViewAll from "./views/Admin/Hospitals/ViewAll.vue";
-import Admin_Hospital_AddNew from "./views/Admin/Hospitals/AddNew.vue";
-import Admin_Hospital_ViewMap from "./views/Admin/Hospitals/ViewMap.vue";
-import Admin_GN_AddNew from "./views/Admin/GramaNiladhari/AddNew.vue";
-import Admin_GN_ViewAll from "./views/Admin/GramaNiladhari/ViewAll.vue";
-import Admin_Donor_ViewAll from "./views/Admin/Donors/ViewAll.vue";
-import Admin_Donor_AddNew from "./views/Admin/Donors/AddNew.vue";
-import Admin_DataEntry_View from "./views/Admin/DataEntry/ViewAll.vue";
+import createpost from "./views/Admin/Information Hub/createpost.vue";
+import show_post from "./views/Admin/Information Hub/show_post.vue";
+import GN_Dashboard from "./views/GramaNiladhari/Dashboard.vue";
+import GramaNiladhari from "./views/GramaNiladhari/GramaNiladhari.vue";
+import GN_Patients from "./views/GramaNiladhari/Patients.vue";
+import GN_Profile from "./views/GramaNiladhari/Profile.vue";
+import Home from "./views/Home/Home.vue";
+import MO_AddPatient from "./views/MedicalOfficer/AddPatient.vue";
+import MO_Dashboard from "./views/MedicalOfficer/Dashboard.vue";
+import MedicalOfficer from "./views/MedicalOfficer/Doctor.vue";
+import MO_Profile from "./views/MedicalOfficer/Profile.vue";
+import MO_ViewAllPatients from "./views/MedicalOfficer/ViewAllPatients.vue";
+import Questionnaire from "./views/Questionnaire/Questionnaire_A.vue";
+import User_Profile from "./views/User/Profile.vue";
 import Admin_DataEntry_AddNew from "./views/Admin/DataEntry/AddNew.vue";
 import Admin_Admins_AddNew from "./views/Admin/DataEntry/AddNew.vue";
+import Admin_DataEntry_View from "./views/Admin/DataEntry/ViewAll.vue";
 import Admin_Admins_View from "./views/Admin/DataEntry/ViewAll.vue";
-
+import Admin_Donor_AddNew from "./views/Admin/Donors/AddNew.vue";
+import Admin_Donor_ViewAll from "./views/Admin/Donors/ViewAll.vue";
+import Admin_GN_AddNew from "./views/Admin/GramaNiladhari/AddNew.vue";
+import Admin_GN_ViewAll from "./views/Admin/GramaNiladhari/ViewAll.vue";
+import Admin_Hospital_AddNew from "./views/Admin/Hospitals/AddNew.vue";
+import Admin_Hospital_ViewAll from "./views/Admin/Hospitals/ViewAll.vue";
+import Admin_Hospital_ViewMap from "./views/Admin/Hospitals/ViewMap.vue";
+import Admin_MO_AddNew from "./views/Admin/MedicalOfficers/AddNew.vue";
+import Admin_MO_ViewAll from "./views/Admin/MedicalOfficers/ViewAll.vue";
+import Admin_Patients_AddNew from "./views/Admin/Patients/AddNew.vue";
+import Admin_Patients_ViewAll from "./views/Admin/Patients/ViewAll.vue";
+import news from "./views/Admin/Information Hub/news.vue"
 Vue.use(Router);
 
 let router = new Router({
@@ -214,6 +218,18 @@ let router = new Router({
         {
           path: "admins/add",
           component: Admin_Admins_AddNew
+        },
+        {
+          path: "Information_hub/create_post",
+          component: createpost
+        },
+        {
+          path: "Information_hub/show_post",
+          component: show_post
+        },
+        {
+          path: "Information_hub/news",
+          component: news
         }
       ]
     }
